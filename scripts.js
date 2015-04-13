67a56fdbd449f4b84986588121be0de41122f0bb
zymaislam <zyma.islam90@gmail.com>
2015-04-12 22:47:17 -0400

    removed entire script code and pasted the Jquery javascript from class
    still not working

--- scripts.js
+++ scripts.js
@@ -1,32 +1,33 @@
-for (var i=0; i<data.length; i++){ //openbrace1
-	
+for (var i=0; i<data.length; i++) {//openbrace1
+
 	var rowData = data[i];
+
+	var $template = $('<div class="descriptionContainer">'+
+	'<div class="mainQuote">'+rowData.titleWhat+'</div>'+
+	'<div class="wholeQuote">'+rowData.answerWhat +'</div>'+
+	'<div class="readMore">Read More</div>'+
+	'<div class="moreInfo">'+
 	
-	var $template =$('<div class="description">'+ 
-				'<div class="titleWhat">'+rowData.titleWhat+'</div>'+
-				'<div class="answerWhat">'+rowData.answerWhat+'</div>'
-				+
-				'<div class="readMore">Read More</div>'+
-				'<div class="moreInfo">'+
-				'<div class="extraInfo">'+rowData.extraInfo+'</div>'
-				+'</div>'
-				+'</div>');
-				
+	'<div class="source">'+rowData.extraInfo+'				</div>'+
+	'</div>'); //changed variable from template to $template to notify myself that I wrapped it
+
 	$template.find(".readMore").on("click", toggleContent);
-	
-	function toggleContent(e) { //openbrace2
-		$descriptionContainer = $(this).parent(); 
-//openbrace3
-		if ($descriptionContainer.hasClass("expanded")) {//you can see content!
+
+	function toggleContent(e) {//openbrace2
+		$descriptionContainer = $(e.target).parent(); 
+		console.log ($quoteContainer)
+		if ($descriptionContainer.hasClass("expanded")) {//openbrace3//you can see content!
 			$descriptionContainer.removeClass("expanded") //this command removes content
 		}//closebrace3
-		 else { //openbrace4
+		 else {//openbrace4
 			$descriptionContainer.addClass("expanded"); //since you cannot see content, this adds content!
-		} //closebrace4
-	} //closebrace2
+		}//openbrace4
+	}//closera
 
-			
+
 	$('#dataContainer').append($template);
 
-	} //closebrace1
+	}
+
+
 
